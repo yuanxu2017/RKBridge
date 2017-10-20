54b6581b1c30e6bf878d1ac7d0247e6f25b00e24
21334878@qq.com <21334878@qq.com>
2017-10-20 20:20:22 +0800

    update

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ const ssdpServer = require('./ssdp-server');
 const PORT = 9999;
 const uuid = require('node-uuid');
 
-function Rokid() {
-    
+let cmdFunc = undefined;
+
+function Rokid(func) {
+    cmdFunc = func;
 }
 // Start Bridge first
-Rokid.prototype.start = function(ip,port,networkEquipment){
-    bridgeServer.start(port || PORT, null, (err, iPort) => {
-  // then start the ssdp server to broad cast your servcie
-    console.log(`tcp://${ip}:${iPort}`)
-    
-  ssdpServer.start({location:`tcp://${ip}:${iPort}`,UUID:uuid.v4(),networkEquipment:networkEquipment});
- });
+Rokid.prototype.start = function(ip,port,devs,networkEquipment){
+    bridgeServer.start(port || PORT, null,devs,this.executeCmd, (err, iPort) => {
+        console.log(`tcp://${ip}:${iPort}`)
+
+        ssdpServer.start({location:`tcp://${ip}:${iPort}`,UUID:uuid.v4(),networkEquipment:networkEquipment});
+    });
+}
+
+Rokid.prototype.executeCmd = function (args,onBind) {
+
+    if(cmdFunc != undefined){
+        if(cmdFunc(args)){
+            onBind(true);
+        }else{
+            onBind(false);
+        }
+    }else {
+        onBind(false);
+    }
+
 }
 
 module.exports = Rokid;
