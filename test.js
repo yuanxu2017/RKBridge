aea8a464586b7faf40dbfaeb150eb30c1d449f10
21334878@qq.com <21334878@qq.com>
2017-10-20 20:20:59 +0800

    update for callback

--- test.js
+++ test.js
@@ -1,4 +1,24 @@
 const ROKID = require('./index')
 
-var main = new ROKID();
-main.start('192.168.0.109','9999','wlp3s0');
+var devices = [];
+for(let i = 0; i< 3; i++) {
+    devices.push({
+        name: 'Device' + i,
+        deviceId: i.toString(),
+        actions: {
+            switch: ['on', 'off']
+        },
+        state: {
+            switch: null
+        },
+        type: 'light'
+    })
+}
+
+function exeCommand(args) {
+    console.log('test args:',JSON.stringify(args));
+    return true;
+}
+
+var main = new ROKID(exeCommand);
+main.start('192.168.0.109','9999',devices,'wlp3s0');
